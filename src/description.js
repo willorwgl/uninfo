9e75a917f7d360fcc456065f2d5e83b81f4f9850
Will Lu <willorwgl@gmail.com>
2019-08-26 23:27:32 -0400

    change path relative to the project repository

--- src/description.js
+++ src/description.js
@@ -7,7 +7,7 @@ export function universityDescription(descriptions, images) {
         .append("div")
         .attr("class", "description")
     debugger
-    const html = `<div><img src=${images[0] ? images[0] :  "./src/assets/images/university.png"} class="university-photo"></img> ${descriptions[0]}</div> ${compare ? `<div> ${descriptions[1]} <img src=${images[1] ? images[1] : "./src/assets/images/university.png"} class="university-photo"></img> </div>` : ""}`
+    const html = `<div><img src=${images[0] ? images[0] :  "../src/assets/images/university.png"} class="university-photo"></img> ${descriptions[0]}</div> ${compare ? `<div> ${descriptions[1]} <img src=${images[1] ? images[1] : "../src/assets/images/university.png"} class="university-photo"></img> </div>` : ""}`
     container.html(html)
 
 }
